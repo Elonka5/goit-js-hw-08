46904fa57efe3fc37d8f885847f71a1feb9b97cd
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-06-14 21:53:17 +0300

    add hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+console.log(Player);
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+function onPlay(data) {
+  localStorage.setItem("videoplayer-current-time", `${data.seconds}`)
+}
+
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
+player.setCurrentTime(Number(localStorage.getItem("videoplayer-current-time")));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputMessage = document.querySelector('.feedback-form textarea');
+const inputEmail = document.querySelector('.feedback-form input');
+const KEY_STORAGE = 'feedback-form-state';
+
+let inputValue = JSON.parse(localStorage.getItem(KEY_STORAGE)) || {};
+console.log(inputValue);
+
+inputEmail.value = inputValue.email || '';
+inputMessage.value = inputValue.message || '';
+
+const savedSettings = throttle(() => {
+  let objects = {
+    email: inputEmail.value,
+    message: inputMessage.value,
+  };
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(objects));
+}, 500);
+
+inputEmail.addEventListener('input', savedSettings);
+inputMessage.addEventListener('input', savedSettings);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  inputEmail.removeEventListener('input', savedSettings);
+  inputMessage.removeEventListener('input', savedSettings);
+  localStorage.removeItem(KEY_STORAGE);
+  inputEmail.value = '';
+  inputMessage.value = '';
+});
